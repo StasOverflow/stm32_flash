0550aa6dd336e65fddcd94ce71015db7a94e6dac
Стрюков Владимир <V.Striukov@Pulse.zp.ua>
2019-01-31 16:56:20 +0200

    working on gui

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -1,5 +1,7 @@
 import time
+from .gui.gui import MainWindow
 
 
 def launch():
     print("henlo, da taim is ", time.time())
+    frame = MainWindow(None, 'Small editor')
--- app/gui/gui.py
+++ app/gui/gui.py
@@ -1,9 +1,18 @@
-import tkinter
+# !/usr/bin/env python
+import wx
 
 
-class MainWindow:
+class MainWindow(wx.Frame):
+    """ We simply derive a new class of Frame. """
 
-    def __init__(self):
-        print('Got a main window init call')
-        pass
+    def __init__(self, parent, title='stm32 loader'):
+        app = wx.App(False)
+        self.style = wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER
+        super().__init__(self, parent, title=title, size=(480, 520), style=self.style)
+
+        self.images = ['tolstoy.jpg', 'feuchtwanger.jpg', 'balzac.jpg', 'pasternak.jpg', 'galsworthy.jpg', 'wolfe.jpg', 'zweig.jpg']
+        authors = ['Leo Tolstoy', 'Lion Feuchtwanger', 'Honore de Balzac', 'Boris Pasternak', 'John Galsworthy', 'Tom Wolfe', 'Stefan Zweig' ]
+
+        wx.ComboBox(self, -1, pos=(50, 170), size=(150, 10), choices=authors, style=wx.CB_READONLY)
+        app.MainLoop()
 
